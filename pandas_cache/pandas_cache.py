a1a8e3e980db65423727c552a8fbcaced8c484dd
Martín Gaitán <gaitan@gmail.com>
2020-07-17 16:24:16 -0300

    consider arguments in the key

--- pandas_cache/pandas_cache.py
+++ pandas_cache/pandas_cache.py
@@ -1,20 +1,20 @@
-""" 
-Implements on disk caching of transformed dataframes 
+"""
+Implements on disk caching of transformed dataframes
 
-Used on a function that returns a single pandas object, 
+Used on a function that returns a single pandas object,
 this decorator will execute the function, cache the dataframe as a pickle
-file using the hash of the raw code as a unique identifier. 
-The next time the function runs, if the hash of the raw code matches 
-what is on disk, the decoratored function will simply load and return 
+file using the hash of the raw code as a unique identifier.
+The next time the function runs, if the hash of the raw code matches
+what is on disk, the decoratored function will simply load and return
 the pickled pandas object.
-This can result in speedups of 10 to 100 times, or more, depending on the 
+This can result in speedups of 10 to 100 times, or more, depending on the
 complexity of the function that creates the dataframe.
 
-If the function changes since the last hash, this decorator will automatically delete 
+If the function changes since the last hash, this decorator will automatically delete
 the previously cached pickle and save a new one, preventing disk pollutionself.
 The caveat is that if the function name changes or the function is deleted, previously
 cached dataframes will remain on disk. For this purpose there is a 'del_cached' function
-that is will simply delete any objects cached by this decorator. 
+that is will simply delete any objects cached by this decorator.
 """
 
 from functools import wraps
@@ -37,11 +37,13 @@ def pd_cache(func):
 
     @wraps(func)
     def cache(*args, **kw):
-        # Get raw code of function as str and hash it
-        func_code = ''.join(inspect.getsourcelines(func)[0]).encode('utf-8')
-        hsh = hashlib.md5(func_code).hexdigest()[:6]
 
-        f = '.pd_cache/' + func.__name__ + '_' + hsh + '.pkl'
+        # Get raw code of function as str and hash it
+        func_code = ''.join(inspect.getsourcelines(func)[0])
+        key = (func_code + repr(args) + repr(kw)).encode('utf8')
+        hsh = hashlib.md5(key).hexdigest()[:6]
+
+        f = f'.pd_cache/{func.__name__}_{hsh}.pkl'
 
         if os.path.exists(f):
             df = pd.read_pickle(f)
